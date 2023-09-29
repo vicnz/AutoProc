803cb66cf76090db44ee3a383342d5e4612fd570
NZ <v.adam.gonzales@gmail.com>
2023-09-29 22:41:31 +0800

    removed

--- src/app/administrator/records/[id]/page.tsx
+++ src/app/administrator/records/[id]/page.tsx
@@ -3,18 +3,18 @@ import { Tabs } from 'antd';
 //components
 import TabCols from './_components/tabs';
 import DocStatus from './_components/status';
-import PrIdContext from './pr-id-context';
+import PRIdProvider from './_components/pr-id-context';
 //configs
 //
 const RecordItem = async function ({ params }: { params: { id: string } }) {
     return (
         <>
-            <PrIdContext id={params.id}>
+            <PRIdProvider id={params.id}>
                 <div style={{ display: 'grid', gridTemplateColumns: '1fr 250px', height: 'calc(100vh - 112px)', width: 'calc(100vw - 56px)' }}>
                     <Tabs tabPosition='left' items={TabCols} defaultActiveKey='pr' destroyInactiveTabPane />
                     <DocStatus />
                 </div>
-            </PrIdContext>
+            </PRIdProvider>
         </>
     )
 }
