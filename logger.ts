dfe1acd5d6a0d582dcfc89ec1c2aa0d52c98d625
NZ <v.adam.gonzales@gmail.com>
2023-10-25 19:27:54 +0800

    Updated Logger

--- logger.ts
+++ logger.ts
@@ -5,16 +5,16 @@ const logger = winston.createLogger({
     level: 'info',
     format: combine(colorize(), timestamp(), json(), metadata()),
     transports: [
-        new winston.transports.File({
-            filename: 'app.log',
-        }),
+        new winston.transports.Console({
+            format: simple()
+        })
     ]
 })
 
 
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: simple()
-    }))
+if (process.env.NODE_ENV === 'production') {
+    logger.add(new winston.transports.File({
+        filename: 'app.log',
+    }),)
 }
 export { logger }
