3e69b3772c644711783095aa44aea456c6655b11
NZ <v.adam.gonzales@gmail.com>
2023-10-25 22:13:14 +0800

    FIXED: Client Side Routing Error ! Antd Style Caching -> [Memoize]

--- src/lib/theme/antd.tsx
+++ src/lib/theme/antd.tsx
@@ -6,7 +6,7 @@ import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs';
 import { useServerInsertedHTML } from 'next/navigation';
 
 const StyledComponentsRegistry = ({ children }: { children: React.ReactNode }) => {
-    const cache = createCache();
+    const cache = React.useMemo(() => createCache(), []);
     useServerInsertedHTML(() => (
         <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }} />
     ));
